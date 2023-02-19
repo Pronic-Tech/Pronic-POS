4b89742867bf01287d3fb47300ff4fb95e7764d6
nickssilver <silvernicks9@gmail.com>
2023-02-19 12:27:46 +0300

    check

--- POS.InventoryManagementSystem/IMS.DataAccess/InventoryProducts/InventoryDBDataAccess.cs
+++ POS.InventoryManagementSystem/IMS.DataAccess/InventoryProducts/InventoryDBDataAccess.cs
@@ -42,7 +42,7 @@ namespace IMS.DataAccess
         //internal DataTable dt
         public InventoryDBDataAccess()
         {
-            this.Sqlcon = new SqlConnection(@"Data Source=X510UQR;Initial Catalog=inventoryDB;Persist Security Info=True;User ID=sa;Password=madworld");
+            this.Sqlcon = new SqlConnection(@"Data Source=PRONIC\Software Engineer;Initial Catalog=PRONICPOS;Integrated Security=True;");
             Sqlcon.Open();
         }
 
